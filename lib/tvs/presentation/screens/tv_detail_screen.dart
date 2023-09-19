e8f42f6736c410f095b0dd1a7aa6e4f1b99442ad
HassanHamdouna <hassan.h.developer@gmail.com>
2023-09-20 00:04:21 +0300

    delet SliverPadding

--- lib/tvs/presentation/screens/tv_detail_screen.dart
+++ lib/tvs/presentation/screens/tv_detail_screen.dart
@@ -191,24 +191,6 @@ class TvDetailScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                    SliverPadding(
-                      padding:
-                          const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 24.0),
-                      sliver: SliverToBoxAdapter(
-                        child: FadeInUp(
-                          from: 20,
-                          duration: const Duration(milliseconds: 500),
-                          child: Text(
-                            'More like this'.toUpperCase(),
-                            style: const TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
                     const SliverPadding(
                         padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 24.0),
                         sliver: TvDetailTabBarScreen()),

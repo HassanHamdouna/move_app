f12a271acd4d4f159e1522b7533222c3abe9dee0
HassanHamdouna <hassan.h.developer@gmail.com>
2023-09-20 23:16:13 +0300

    set preferred orientation - portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:move_app/core/global/theme/theme_data/theme_data_dark.dart';
 import 'package:move_app/core/services/services_locator.dart';
 import 'package:move_app/core/global/localization/app_strings.dart';
 import 'app/screen/bottom_navigation_bar_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]
+  );
   ServicesLocator().init();
   runApp(const MyApp());
 }
